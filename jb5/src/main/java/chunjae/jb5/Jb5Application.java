fc522bf4ef00aa97e2ae38aef48e883d685e3c02
ehdbs774 <ehdbs774@gmail.com>
2022-12-01 16:45:37 +0900

    jb5 폴더 생성하였습니다.

---
+++ jb5/src/main/java/chunjae/jb5/Jb5Application.java
@@ -0,0 +1,13 @@
+package chunjae.jb5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jb5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jb5Application.class, args);
+	}
+
+}
---
+++ jb5/src/test/java/chunjae/jb5/Jb5ApplicationTests.java
@@ -0,0 +1,13 @@
+package chunjae.jb5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jb5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
